0ed605428873769e0d5155b6a8794a17ea7bbb36
grzegorz <grzegorz.spayda@webtailor.pl>
2022-08-04 10:24:40 +0200

    feat(exception): adding exception handling

--- src/Controller/ParkingSpotController.php
+++ src/Controller/ParkingSpotController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\ParkingSpot;
 use App\Repository\ParkingSpotRepository;
+use App\Service\ServiceException;
+use App\Service\ServiceExceptionData;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,35 +14,71 @@ class ParkingSpotController extends AbstractController
 {
 
     public function __construct(
-        private ParkingSpotRepository $repository
+        private readonly ParkingSpotRepository $repository
     )
     {
     }
 
+    /**
+     * @throws \Exception
+     */
     #[Route('/parking-spot/generator', methods: 'POST')]
-    public function generator()
+    public function generator(): JsonResponse
     {
-        try {
+        throw new ServiceException(new ServiceExceptionData(404, 'Parking spot generator is disabled'));
 
-            $parkingSpot = $this->createParkingSpot();
+        $result = $this->createParkingSpotGarage(114, 1, 'G3', '19E');
 
-            $this->repository->add($parkingSpot);
-        } catch (\Exception $exception) {
-            throw new \Exception($exception->getMessage(), $exception->getCode());
+        if (!$result) {
+            throw new \Exception('Could not generate G3');
+        }
+
+        $result = $this->createParkingSpotGarage(163, 1, 'G2', '19');
+
+        if (!$result) {
+            throw new \Exception('Could not generate G2');
+        }
+
+        $result = $this->createParkingSpotGarage(149, 2, 'G1', '22');
+
+        if (!$result) {
+            throw new \Exception('Could not generate G1');
         }
 
         return new JsonResponse(data: 'data generated successfully', json: true);
     }
 
+    /**
+     * @throws \Exception
+     */
+    private function createParkingSpotGarage(int $spotNumber, string $stage, string $garage, string $nearestStaircase): bool
+    {
+        try {
+
+            for ($i = 1; $i <= $spotNumber; $i++) {
+
+                $parkingSpot = $this->createParkingSpot();
+
+                $parkingSpot->setStage($stage);
+                $parkingSpot->setGarage($garage);
+                $parkingSpot->setNearestStaircase($nearestStaircase);
+
+                $parkingSpot->setSpot(1);
+                $parkingSpot->setCode($parkingSpot->getGarage() . '.' . $parkingSpot->getSpot());
+
+                $this->repository->add($parkingSpot, $i === $spotNumber);
+            }
+        }
+        catch (\Exception $exception) {
+            throw new \Exception($exception->getMessage(), $exception->getCode());
+        }
+        return true;
+    }
+
     private function createParkingSpot(): ParkingSpot
     {
         $parkingSpot = new ParkingSpot();
-        $parkingSpot->setStage(1);
-        $parkingSpot->setGarage('G3');
-        $parkingSpot->setSpot(1);
-        $parkingSpot->setCode($parkingSpot->getGarage() . '.' . $parkingSpot->getSpot());
         $parkingSpot->setAvailable(false);
-        $parkingSpot->setNearestStaircase('19E');
 
         return $parkingSpot;
     }
